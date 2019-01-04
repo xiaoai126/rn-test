315f7f4d9e866fe7a58a893005dbb3c1d59fed36
oem <oem@oem.local>
2019-01-05 01:42:30 +0800

    init app

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+export default class App extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to React Native!</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
