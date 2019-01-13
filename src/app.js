0df82997ca3f64b270e1a080acb5029e04dc9a59
oem <oem@oem.local>
2019-01-13 23:28:06 +0800

    location

--- src/app.js
+++ src/app.js
@@ -6,24 +6,42 @@ class App extends Component{
     title: 'App',
     headerBackTitle: null,
   })
+  state = {
+    longitude: '',
+    latitude: '',
+    location: 'loading',
+    cityLocation: ''
+  }
   componentDidMount() {
     navigator.geolocation.watchPosition(
       (position) => {
         let longitude = JSON.stringify(position.coords.longitude);//精度
         let latitude = JSON.stringify(position.coords.latitude);//纬度
         console.warn(`精度：${longitude}, 纬度：${latitude}`);
-        // this.fetchData(longitude,latitude);
+        this.setState({
+          location: 'city',
+          cityLocation: `精度：${longitude}, 纬度：${latitude}`,
+        })
+
       },
       (error) =>{
-        console.log(error);
+        console.warn(`定位失败： ${JSON.stringify(error)}`);
+        this.setState({
+          location: 'error',
+          cityLocation: `定位失败： ${JSON.stringify(error)}`,
+        })
       },
       {enableHighAccuracy: true, timeout: 5000, maximumAge: 1000}
       )
   }
   render() {
+    const { location, cityLocation } = this.state
     return (
       <View style={styles.container}>
         <Text style={{textAlign: 'center'}}>App Page Test</Text>
+          {location == 'loading' ? <Text style={{textAlign: 'center'}}>正在定位中，请耐心等待...</Text> : null }
+          {location === 'city' ? <Text style={{textAlign: 'center'}}>定位成功： {cityLocation}</Text> : null }
+          {location === 'error' ? <Text style={{textAlign: 'center'}}>定位失败...</Text> : null }
         <Button
           title={'go to Home'}
           onPress={() => {this.props.navigation.navigate('Home')}}
